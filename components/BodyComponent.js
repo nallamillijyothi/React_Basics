df2c4c8941422cfcb2ceb1aa41e12d0ddedf3332
Jyothi14R11A0596 <jyothi.n@c2stechs.com>
2024-01-09 11:35:39 +0530

    used useEffect to make api call to real swiggy api, updated search functionality, and implemented shimmer UI

--- components/BodyComponent.js
+++ components/BodyComponent.js
@@ -1,33 +1,50 @@
 import RestoCardComponent from "./RestoCardComponent";
-import { restoList, carousalList } from "../utils/mockData";
+import { carousalList } from "../utils/mockData";
 import Search from "./Search";
-import { useState } from "react";
-
-
+import { useEffect, useState } from "react";
+import ShimmerUI from "./ShimmerUI";
 
 const BodyComponent = () => {
-const [restoOList, setRestoList] = useState(restoList)
-  function searchItem(data){
-    console.log(restoOList)
-    if(data != ''){
+  const [restoOList, setRestoList] = useState([]);
+
+  useEffect(() => {
+    fetchRestoData();
+  }, []);
+
+  const fetchRestoData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4556724&lng=78.3991533&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTI"
+    );
+    const jsonData = await data.json();
+    setRestoList(
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  function searchItem(data) {
+    if (data != "") {
       str = data.toLowerCase();
       const dataa = restoOList.filter((item) => {
-        return item.cuisines.some(cuisine => {
-          return cuisine.toLowerCase().includes(str) || item.name.toLowerCase().includes(str)
+        return item.info.cuisines.some((cuisine) => {
+          return (
+            cuisine.toLowerCase().includes(str) ||
+            item.info.name.toLowerCase().includes(str)
+          );
         });
-      })
-       setRestoList(dataa)
-    }
-    else{
-      setRestoList(restoList)
+      });
+      setRestoList(dataa);
+    } else {
+      fetchRestoData();
     }
   }
+
   return (
     <div className="body_container">
       <div className="body_heading__container">
-      <div className="body_search_container">
-          <Search onSearch={searchItem}/>
-      </div>
+        <div className="body_search_container">
+          <Search onSearch={searchItem} />
+        </div>
         <h2>What's on your mind?</h2>
         <div className="body_list_container">
           <ul className="menu_items__list">
@@ -51,13 +68,17 @@ const [restoOList, setRestoList] = useState(restoList)
         </div>
         <div className="body_resto_list__container">
           <ul className="body_resto_list">
-            {restoOList.map((restaurant) => {
-              return (
-                <li className="body_resto_list_item" key={restaurant.id}>
-                  <RestoCardComponent restoData={restaurant} />
-                </li>
-              );
-            })}
+            {restoOList.length === 0 ? (
+              <ShimmerUI />
+            ) : (
+              restoOList.map((restaurant) => {
+                return (
+                  <li className="body_resto_list_item" key={restaurant.info.id}>
+                    <RestoCardComponent restoData={restaurant.info} />
+                  </li>
+                );
+              })
+            )}
           </ul>
         </div>
       </div>
---
+++ components/ShimmerUI.js
@@ -0,0 +1,71 @@
+const ShimmerUI = () => {
+  return (
+    <>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+      <div className="shimmer_container">
+        <div className="shimmer__card">
+          <div className="shimmer__card_img"></div>
+          <div className="shimmer__card_details">
+            <div className="shimmer_card_title"></div>
+            <div className="shimmer_card_location"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ShimmerUI;
